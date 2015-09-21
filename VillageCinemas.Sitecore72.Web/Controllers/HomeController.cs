fe5f0cc71024f1d4fdeaeb43fd5f3a6f0d7b0982
clone278 <1one5t4r>
2015-09-21 21:16:27 +0000

    Test 1

--- VillageCinemas.Sitecore72.Web/Controllers/HomeController.cs
+++ VillageCinemas.Sitecore72.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace VillageCinemas.Sitecore72.Web.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page. 2";
 
             return View();
         }
