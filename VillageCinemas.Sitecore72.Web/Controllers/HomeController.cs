72c820892bc02472774a3086b3f41a74192c366e
clone278 <1one5t4r>
2015-09-22 05:49:27 +0000

    Changes develop branch

--- VillageCinemas.Sitecore72.Web/Controllers/HomeController.cs
+++ VillageCinemas.Sitecore72.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace VillageCinemas.Sitecore72.Web.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.4";
+            ViewBag.Message = "Your contact page.5";
 
             return View();
         }
